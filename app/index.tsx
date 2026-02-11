727684c1b6622356e781fcaef683137473663f3d
Syreese Delos Santos <syreeseofficial@gmail.com>
2026-02-11 11:02:36 -0800

    feat: setup basic layout with expo-router

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Loading...</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#121212',
+    },
+    text: {
+        color: '#FFFFFF',
+        fontSize: 18,
+    },
+});
